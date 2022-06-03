4380f1896e57411fef0463427bdc415efc50b4b4
Ben Gosney <bengosney@googlemail.com>
2022-06-03 21:57:10 +0100

    Test the new choice length

--- crm/tests/test_choice_length.py
+++ crm/tests/test_choice_length.py
@@ -1,15 +1,16 @@
 # Django
+from django.db import models
 from django.test import TestCase
 
 # Locals
-from .utils import max_length
+from ..utils import choice_length
 
 
 class test_choice_length(TestCase):
     def test_max_length(self):
-        class testLength:
+        class testLength(models.TextChoices):
             ONE = "one", "One"
             TWO = "two", "Two"
             THREE = "three", "Three"
 
-        self.assertEqual(max_length(testLength), 3)
+        self.assertEqual(choice_length(testLength), 5)
--- crm/utils.py
+++ crm/utils.py
@@ -2,5 +2,5 @@
 from django.db import models
 
 
-def max_length(choices: models.TextChoices) -> int:
-    return max(len(choice[0]) for choice in choices)
+def choice_length(choices: type[models.TextChoices]) -> int:
+    return max(len(choice[0]) for choice in choices.choices)
