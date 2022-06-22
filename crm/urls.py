74e1846426e8a21188bcc63c9e956bfd1aee54d9
Ben Gosney <bengosney@googlemail.com>
2022-06-22 19:26:27 +0100

    Fix url namespace overlap

--- crm/urls.py
+++ crm/urls.py
@@ -7,7 +7,7 @@ from .views import CustomerEditView, CustomerListView, PetListView, VetListView
 
 urlpatterns = [
     path("api/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework_auth")),
     path("customers/<int:pk>/", CustomerEditView.as_view(), name="customer_edit"),
     path("customers/", CustomerListView.as_view(), name="customers"),
     path("pets/", PetListView.as_view(), name="pets"),
