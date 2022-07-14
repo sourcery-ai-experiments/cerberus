61db6fbcef2d67df63d847a03f6bef9dd568d600
Ben Gosney <bengosney@googlemail.com>
2022-07-14 11:02:39 +0100

    Swap to page number pagination

--- cerberus/settings.py
+++ cerberus/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.BrowsableAPIRenderer",
         "rest_framework.renderers.TemplateHTMLRenderer",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.CursorPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
 
