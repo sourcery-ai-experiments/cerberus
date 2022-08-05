7c4ea8ad981b07c4b2193f6ba79f726c97055fd3
Ben Gosney <bengosney@googlemail.com>
2022-08-05 08:36:19 +0100

    Remove a load of unused stuff

--- cerberus/settings.py
+++ cerberus/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "crm.context_processors.nav_links",
             ],
         },
     },
--- crm/urls.py
+++ crm/urls.py
@@ -4,15 +4,10 @@ from django.urls import include, path
 
 # Locals
 from .api import router
-from .views import CustomerEditView, CustomerListView, PetListView, VetListView
 
 urlpatterns = [
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework_auth")),
-    path("customers/<int:pk>/", CustomerEditView.as_view(), name="customer_edit"),
-    path("customers/", CustomerListView.as_view(), name="customers"),
-    path("pets/", PetListView.as_view(), name="pets"),
-    path("vets/", VetListView.as_view(), name="vets"),
 ]
 
 if settings.DEBUG:
