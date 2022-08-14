4f221642f27ff84909eae0c237b206ccbe23ba5e
Ben Gosney <bengosney@googlemail.com>
2022-08-14 14:10:13 +0100

    Configure static files

--- cerberus/settings.py
+++ cerberus/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "cerberus" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
