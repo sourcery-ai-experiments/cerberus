9df8e78b6938c208b3ec32410ca92602eec67be2
Ben Gosney <bengosney@googlemail.com>
2022-09-06 14:02:23 +0100

    Add the OpenAPIv3 schema views, fixes #18

--- cerberus/urls.py
+++ cerberus/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.urls import include, path
 
 # Third Party
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # Locals
@@ -14,6 +15,9 @@ urlpatterns = [
     path("api-auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework_auth")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("", include(api.router.urls)),
     path("", include(api.urls)),
 ]
