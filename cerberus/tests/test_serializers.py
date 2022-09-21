544e61ca981b288b68d9716b9e2509d0b9a2d1f6
Ben Gosney <bengosney@googlemail.com>
2022-09-21 14:05:28 +0100

    Basic customer can serializer test

---
+++ cerberus/tests/test_serializers.py
@@ -0,0 +1,51 @@
+# Standard Library
+import random
+
+# Django
+from django.test import TestCase
+
+# Third Party
+from model_bakery import baker
+from moneyed import Money
+
+# Locals
+from ..models import Address, Contact, Customer, Pet
+from ..serializers import ContactSerializer, CustomerSerializer
+
+baker.generators.add("djmoney.models.fields.MoneyField", lambda: Money(random.uniform(1.0, 100.0), "GBP"))
+
+
+class SerializerTests(TestCase):
+    def test_contact(self):
+        contact: Contact = baker.make(Contact)
+        data = ContactSerializer(instance=contact).data
+
+        validation = {
+            "id": contact.pk,
+            "type": contact.type.value,
+            "name": contact.name,
+            "details": contact.details,
+        }
+
+        self.assertDictEqual(data, validation)
+
+    def test_customer(self):
+        pets: list[Pet] = baker.make(Pet, _quantity=3)
+        addresses: list[Address] = baker.make(Address, _quantity=3)
+        contacts: list[Contact] = baker.make(Contact, _quantity=3)
+
+        customer: Customer = baker.make(Customer, pets=pets, addresses=addresses, contacts=contacts)
+
+        try:
+            data = CustomerSerializer(instance=customer).data
+        except Exception as E:
+            self.fail(f"Exception raised: {E}")
+
+        validation = {
+            "id": customer.pk,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "name": customer.name,
+        }
+
+        self.assertDictContainsSubset(validation, data)
