ce7cf96f95bd4530ae0becac2da48072859465b7
Ben Gosney <bengosney@googlemail.com>
2023-06-28 16:07:36 +0100

    Deleting and auto delete success url

--- cerberus/views.py
+++ cerberus/views.py
@@ -1,16 +1,19 @@
 # Standard Library
 from enum import Enum
+from typing import Any
 
 # Django
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Model
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.utils.decorators import classonlymethod
 
 # Third Party
-from vanilla import CreateView, DeleteView, DetailView, GenericModelView, ListView, UpdateView
+from vanilla import CreateView
+from vanilla import DeleteView as DeleteView
+from vanilla import DetailView, GenericModelView, ListView, UpdateView
 
 # Locals
 from .forms import CustomerForm
@@ -32,12 +35,19 @@ class Actions(Enum):
 
 class CRUDViews(GenericModelView):
     model = Model
+    delete_success_url: str | None = None
 
     @classonlymethod
-    def get_defaults(cls):
-        return {
+    def get_defaults(cls, action: Actions) -> dict[str, Any]:
+        defaults: dict[str, Any] = {
             "paginate_by": 25,
         }
+
+        match action:
+            case Actions.DELETE:
+                defaults["success_url"] = cls.delete_success_url or reverse_lazy(f"{cls.model._meta.model_name}_list")
+
+        return defaults
 
     @classonlymethod
     def as_view(cls, action: Actions):
@@ -47,12 +57,13 @@ class CRUDViews(GenericModelView):
                 LoginRequiredMixin,
                 action.value,
             ),
-            {**cls.get_defaults(), **dict(cls.__dict__)},
+            {**cls.get_defaults(action), **dict(cls.__dict__)},
         ).as_view()
 
     @classonlymethod
     def get_urls(cls):
         model_name = cls.model._meta.model_name
+
         urlpatterns = [
             path(f"{model_name}/", cls.as_view(action=Actions.LIST), name=f"{model_name}_list"),
             path(f"{model_name}/new/", cls.as_view(action=Actions.CREATE), name=f"{model_name}_create"),
