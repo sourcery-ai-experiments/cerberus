50d95b5a6c6c22b796f2065da640752bb98b87ff
Ben Gosney <bengosney@googlemail.com>
2024-02-08 21:36:03 +0000

    improve some of the tests

--- cerberus/tests/test_contacts.py
+++ cerberus/tests/test_contacts.py
@@ -1,35 +1,47 @@
 # Django
-from django.test import TestCase
+
+# Third Party
+import pytest
 
 # Locals
 from ..models import Contact
 
 
-class test_contacts(TestCase):
-    def test_phone_type(self):
-        numbers = [
-            "+441234567890",
-            "01234567890",
-            "(01234)567890",
-            "01234 567890",
-        ]
+def numbers():
+    yield "+441234567890"
+    yield "01234567890"
+    yield "(01234)567890"
+    yield "01234 567890"
 
-        for number in numbers:
-            contact = Contact(details=number)
-            self.assertEqual(contact.type, Contact.Type.PHONE)
 
-    def test_mobile_type(self):
-        numbers = [
-            "+447234567890",
-            "07234567890",
-            "(07234)567890",
-            "07234 567890",
-        ]
+@pytest.mark.parametrize("number", numbers())
+def test_phone_type(number):
+    contact = Contact(details=number)
+    assert contact.type == Contact.Type.PHONE
 
-        for number in numbers:
-            contact = Contact(details=number)
-            self.assertEqual(contact.type, Contact.Type.MOBILE)
 
-    def test_email_type(self):
-        contact = Contact(details="bob@example.com")
-        self.assertEqual(contact.type, Contact.Type.EMAIL)
+def mobile_numbers():
+    yield "+447234567890"
+    yield "07234567890"
+    yield "(07234)567890"
+    yield "07234 567890"
+
+
+@pytest.mark.parametrize("number", mobile_numbers())
+def test_mobile_type(number):
+    contact = Contact(details=number)
+    assert contact.type == Contact.Type.MOBILE
+
+
+def emails():
+    yield "blueberry1234@example.com"
+    yield "sunflower_87@mail.com"
+    yield "pineapplemaster456@hotmail.com"
+    yield "techwizard789@gmail.com"
+    yield "rainbowsparkle22@yahoo.com"
+
+
+@pytest.mark.parametrize("email", emails())
+def test_email_type(email):
+    contact = Contact(details=email)
+    assert contact.type == Contact.Type.EMAIL
--- cerberus/tests/test_customer.py
+++ cerberus/tests/test_customer.py
@@ -1,10 +1,8 @@
 # Standard Library
 import random
 
-# Django
-from django.test import TestCase
-
 # Third Party
+import pytest
 from model_bakery import baker
 from moneyed import Money
 
@@ -14,9 +12,11 @@ from ..models import Customer
 baker.generators.add("djmoney.models.fields.MoneyField", lambda: Money(random.uniform(1.0, 100.0), "GBP"))
 
 
-class InvoiceTests(TestCase):
-    def setUp(self) -> None:
-        self.customer: Customer = baker.make(Customer, invoice_email="test@example.com")
+@pytest.fixture
+def customer():
+    yield baker.make(Customer)
 
-    def test_full_name(self):
-        self.assertEqual(self.customer.name, f"{self.customer.first_name} {self.customer.last_name}")
+
+@pytest.mark.django_db
+def test_full_name(customer):
+    assert customer.name == f"{customer.first_name} {customer.last_name}"
