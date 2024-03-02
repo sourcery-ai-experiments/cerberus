b25d4cf294213bfdad23df81e8313193e90f5073
Ben Gosney <bengosney@googlemail.com>
2024-03-02 21:25:19 +0000

    simple tag that imports a template, renders a block and passes it to that template

---
+++ cerberus/templatetags/components.py
@@ -0,0 +1,45 @@
+# Django
+from django import template
+from django.template import Node
+from django.template.library import InclusionNode
+
+register = template.Library()
+
+
+def unquote(value: str) -> str:
+    return value.strip('"').strip("'")
+
+
+class ComponentNode(Node):
+    def __init__(self, node_list, template_name: str, extra_context: dict[str, str] | None = None):
+        self.node_list = node_list
+        self.extra_context = extra_context or {}
+        self.template_name = template_name
+
+    def render(self, context) -> str:
+        self.extra_context["component_block"] = self.node_list.render(context)
+
+        inclusion_node = InclusionNode(
+            lambda c: self.extra_context,
+            args=[],
+            kwargs={},
+            takes_context=True,
+            filename=self.template_name,
+        )
+        return inclusion_node.render(context)
+
+
+@register.tag
+def component(parser, token) -> Node:
+    node_list = parser.parse(("endcomponent",))
+    parser.delete_first_token()
+
+    args = token.split_contents()[1:]
+    template_name = unquote(args[0])
+
+    context = {}
+    for arg in args[1:]:
+        key, value = arg.split("=")
+        context[f"component_{unquote(key)}"] = unquote(value)
+
+    return ComponentNode(node_list, template_name, context)
