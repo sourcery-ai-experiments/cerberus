8484ccea2d067621bf77386294fd3a55119bf25d
Ben Gosney <bengosney@googlemail.com>
2024-03-17 10:31:17 +0000

    Remove dep on dateutil and fix shadowing

--- cerberus/views/booking.py
+++ cerberus/views/booking.py
@@ -1,17 +1,14 @@
 # Standard Library
+import datetime as dt
 from calendar import MONDAY, Calendar, month_name
 from collections import defaultdict, namedtuple
 from collections.abc import Iterable
-from datetime import date, datetime, time, timedelta
 
 # Django
 from django.db.models import Count
 from django.http import Http404
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
-
-# Third Party
-from dateutil.relativedelta import relativedelta
 
 # Locals
 from ..forms import BookingForm
@@ -32,8 +29,8 @@ class BookingCalenderRedirect(RedirectView):
     pattern_name = "booking_calender_month"
 
     def get_redirect_url(self, *args, **kwargs):
-        kwargs["year"] = datetime.now().year
-        kwargs["month"] = datetime.now().month
+        kwargs["year"] = dt.datetime.now().year
+        kwargs["month"] = dt.datetime.now().month
 
         return super().get_redirect_url(*args, **kwargs)
 
@@ -78,7 +75,7 @@ class BookingCalenderYear(TemplateView, CalendarBreadCrumbs):
         context = super().get_context_data(year=year, **kwargs)
 
         try:
-            date = datetime(year=year, month=1, day=1)
+            date = dt.datetime(year=year, month=1, day=1)
         except ValueError as e:
             raise Http404(f"year {year} not found") from e
 
@@ -96,16 +93,16 @@ class BookingCalenderYear(TemplateView, CalendarBreadCrumbs):
 class BookingCalenderMonth(TemplateView, CalendarBreadCrumbs):
     template_name = "cerberus/booking_calender_month.html"
 
-    def grouped_bookings(self, start: date, end: date) -> dict[date, list[Booking]]:
+    def grouped_bookings(self, start: dt.date, end: dt.date) -> dict[dt.date, list[Booking]]:
         bookings = Booking.objects.filter(start__gte=start, end__lte=end).order_by("start")
 
-        bookings_by_date: dict[date, list[Booking]] = defaultdict(list)
+        bookings_by_date: dict[dt.date, list[Booking]] = defaultdict(list)
         for booking in bookings:
             bookings_by_date[booking.start.date()].append(booking)
 
         return bookings_by_date
 
-    def organize_bookings(self, dates: list[date]) -> Iterable[BookingGroup]:
+    def organize_bookings(self, dates: list[dt.date]) -> Iterable[BookingGroup]:
         bookings_by_date = self.grouped_bookings(dates[0], dates[-1])
 
         return [BookingGroup(date, bookings_by_date[date]) for date in dates]
@@ -114,7 +111,7 @@ class BookingCalenderMonth(TemplateView, CalendarBreadCrumbs):
         context = super().get_context_data(**kwargs)
 
         try:
-            date = datetime(year=year, month=month, day=1)
+            date = dt.datetime(year=year, month=month, day=1)
         except ValueError as e:
             raise Http404(f"month {month} not found") from e
 
@@ -123,8 +120,8 @@ class BookingCalenderMonth(TemplateView, CalendarBreadCrumbs):
         context["date"] = date
         context["year"] = year
         context["month"] = month
-        context["next_month"] = date + relativedelta(months=1)
-        context["prev_month"] = date + relativedelta(months=-1)
+        context["next_month"] = (date + dt.timedelta(days=32)).replace(day=1)
+        context["prev_month"] = (date + dt.timedelta(days=-1)).replace(day=1)
         context["calendar"] = self.organize_bookings(list(calendar.itermonthdates(year, month)))
         context["today"] = date.today()
         context["days"] = calendar.iterweekdays()
@@ -137,26 +134,26 @@ class BookingCalenderDay(TemplateView, CalendarBreadCrumbs):
     template_name = "cerberus/booking_calender_day.html"
 
     @staticmethod
-    def add_time(time: time, delta: timedelta) -> time:
-        return (datetime.combine(date.today(), time) + delta).time()
+    def add_time(time: dt.time, delta: dt.timedelta) -> dt.time:
+        return (dt.datetime.combine(dt.date.today(), time) + delta).time()
 
     @classmethod
-    def get_times(cls, start: time, end: time, step: timedelta) -> Iterable[time]:
-        cur: time = start
+    def get_times(cls, start: dt.time, end: dt.time, step: dt.timedelta) -> Iterable[dt.time]:
+        cur: dt.time = start
         while cur < end:
             yield cur
-            cur: time = cls.add_time(cur, step)
+            cur: dt.time = cls.add_time(cur, step)
 
-    def grouped_bookings(self, start: datetime, end: datetime) -> dict[time, list[Booking]]:
+    def grouped_bookings(self, start: dt.datetime, end: dt.datetime) -> dict[dt.time, list[Booking]]:
         bookings = Booking.objects.filter(start__gte=start, end__lte=end).order_by("start")
 
-        bookings_by_date: dict[time, list[Booking]] = defaultdict(list)
+        bookings_by_date: dict[dt.time, list[Booking]] = defaultdict(list)
         for booking in bookings:
             bookings_by_date[booking.start.time()].append(booking)
 
         return bookings_by_date
 
-    def organize_bookings(self, times: list[datetime]) -> Iterable[BookingGroup]:
+    def organize_bookings(self, times: list[dt.datetime]) -> Iterable[BookingGroup]:
         bookings_by_date = self.grouped_bookings(times[0], times[-1])
 
         return [BookingGroup(t, bookings_by_date[t.time()]) for t in times]
@@ -165,7 +162,7 @@ class BookingCalenderDay(TemplateView, CalendarBreadCrumbs):
         context = super().get_context_data(**kwargs)
 
         try:
-            date = datetime(year=year, month=month, day=day)
+            date = dt.datetime(year=year, month=month, day=day)
         except ValueError as e:
             raise Http404("date not found") from e
 
@@ -173,7 +170,7 @@ class BookingCalenderDay(TemplateView, CalendarBreadCrumbs):
         start = 6
         end = 19
         steps = range(0, (end - start) * (60 // step))
-        times: list[datetime] = [date + timedelta(hours=start, minutes=15 * i) for i in steps]
+        times: list[dt.datetime] = [date + dt.timedelta(hours=start, minutes=15 * i) for i in steps]
 
         context["date"] = date
         context["year"] = year
