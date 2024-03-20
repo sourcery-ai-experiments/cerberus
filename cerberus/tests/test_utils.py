e2630430604cd3dd67d03507b42f902365d2adaa
Ben Gosney <bengosney@googlemail.com>
2024-03-20 12:13:26 +0000

    Test the whitespace helper

--- cerberus/tests/test_utils.py
+++ cerberus/tests/test_utils.py
@@ -10,7 +10,7 @@ from django.utils.timezone import is_aware
 import pytest
 
 # Locals
-from ..utils import choice_length, make_aware
+from ..utils import choice_length, make_aware, minimize_whitespace
 
 
 def test_max_length():
@@ -55,3 +55,45 @@ def test_make_aware_called_twice():
 def test_make_does_not_modify_timezone():
     value = make_aware(date(2022, 1, 1), timezone=zoneinfo.ZoneInfo(key="America/New_York"))
     assert value.tzinfo == zoneinfo.ZoneInfo(key="America/New_York")
+
+
+def test_minimize_whitespace_removes_leading_whitespace():
+    value = "    This is a test string"
+    expected = "This is a test string"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_removes_trailing_whitespace():
+    value = "This is a test string    "
+    expected = "This is a test string"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_removes_newlines():
+    value = "This is a test\nstring"
+    expected = "This is a teststring"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_removes_carriage_returns():
+    value = "This is a test\rstring"
+    expected = "This is a teststring"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_removes_multiple_newlines():
+    value = "This is a test\n\nstring"
+    expected = "This is a teststring"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_removes_multiple_carriage_returns():
+    value = "This is a test\r\rstring"
+    expected = "This is a teststring"
+    assert minimize_whitespace(value) == expected
+
+
+def test_minimize_whitespace_with_empty_string():
+    value = ""
+    expected = ""
+    assert minimize_whitespace(value) == expected
