a5c78cb2e747c6ad90f193686c13d3f6f24f17e8
Ben Gosney <bengosney@googlemail.com>
2024-04-07 11:23:11 +0100

    Support for post with variables for the transition view

--- cerberus/views/transition_view.py
+++ cerberus/views/transition_view.py
@@ -1,4 +1,6 @@
 # Standard Library
+import inspect
+from collections.abc import Callable
 
 # Django
 from django.db.models import Model
@@ -11,8 +13,9 @@ class TransitionView(View):
     model = Model
     field = str
 
-    def get(self, request, pk: int, action: str):
+    def get_valid_model(self, pk: int, action: str):
         model = get_object_or_404(self.model, pk=pk)
+
         transitions = getattr(model, f"get_all_{self.field}_transitions")()
         available_transitions = getattr(model, f"get_all_{self.field}_transitions")()
 
@@ -22,7 +25,27 @@ class TransitionView(View):
         if action not in [t.name for t in available_transitions]:
             raise Http404(f"{action} is not currently available on {self.model._meta.model_name}")
 
+        return model
+
+    def get(self, request, pk: int, action: str):
+        model = self.get_valid_model(pk, action)
+
         getattr(model, action)()
+        redirect_url = getattr(model, "get_absolute_url", lambda: "/")()
+
+        return redirect(redirect_url)
+
+    def post(self, request, pk: int, action: str):
+        model = self.get_valid_model(pk, action)
+
+        action_function: Callable = getattr(model, action)
+
+        args = {}
+        for name, _ in inspect.signature(action_function).parameters.items():
+            if name in request.POST:
+                args[name] = request.POST[name]
+
+        action_function(**args)
         redirect_url = getattr(model, "get_absolute_url", lambda: "/")()
 
         return redirect(redirect_url)
