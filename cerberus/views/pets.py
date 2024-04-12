d5cae6939fe084473ce8f871f29f82eee307c98f
Ben Gosney <bengosney@googlemail.com>
2024-04-12 20:41:07 +0100

    WIP: better pet form

--- cerberus/views/pets.py
+++ cerberus/views/pets.py
@@ -1,10 +1,39 @@
 # Django
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+# Third Party
+from vanilla import CreateView
 
 # Locals
 from ..filters import PetFilter
 from ..forms import PetForm
 from ..models import Pet
-from .crud_views import CRUDViews
+from .crud_views import Actions, CRUDViews
+
+
+class PetCreateView(CreateView):
+    model = Pet
+    form_class = PetForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        customer = get_object_or_404(Pet, pk=kwargs["pk"])
+        context = self.get_context_data(form=form, customer=customer)
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        customer = get_object_or_404(Pet, pk=kwargs["pk"])
+        form = self.get_form(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            self.object = form.save(commit=False)
+            self.object.customer = customer
+            self.object.save()
+            form.save_m2m()
+            return HttpResponseRedirect(self.get_success_url())
+
+        context = self.get_context_data(form=form, customer=customer)
+        return self.render_to_response(context)
 
 
 class PetCRUD(CRUDViews):
@@ -12,3 +41,14 @@ class PetCRUD(CRUDViews):
     form_class = PetForm
     filter_class = PetFilter
     sortable_fields = ["name", "customer"]
+
+    url_parts = {
+        **CRUDViews.url_parts,
+        **{Actions.CREATE: "create-for-customer/<int:pk>/"},
+    }
+
+    @classmethod
+    def get_view_class(cls, action: Actions):
+        if action == Actions.CREATE:
+            return PetCreateView
+        return super().get_view_class(action)
