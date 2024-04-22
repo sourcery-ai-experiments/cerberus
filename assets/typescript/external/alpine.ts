4e411f75296061af82d8a17fc1ece0ad4ffef18e
Ben Gosney <bengosney@googlemail.com>
2024-04-22 21:38:10 +0100

    Add alpine morph to try and stop the flicker

--- assets/typescript/external/alpine.ts
+++ assets/typescript/external/alpine.ts
@@ -2,10 +2,12 @@ import Alpine from 'alpinejs';
 
 import collapse from '@alpinejs/collapse'
 import focus from '@alpinejs/focus'
+import morph from '@alpinejs/morph'
 import mask from '@alpinejs/mask'
 
 Alpine.plugin(collapse);
 Alpine.plugin(focus);
+Alpine.plugin(morph);
 Alpine.plugin(mask);
 
 declare global {
