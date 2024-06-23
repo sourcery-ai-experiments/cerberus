33e2ff09dd7b2f9f9446defe4a2c0ed72379cc0c
Ben Gosney <bengosney@googlemail.com>
2024-06-23 20:48:26 +0100

    feat(CheckboxTable): support for overriding column names

--- cerberus/widgets.py
+++ cerberus/widgets.py
@@ -154,9 +154,9 @@ class CheckboxTable(forms.CheckboxSelectMultiple):
 
     crispy_template = "cerberus/widgets/checkbox_table.html"
 
-    def __init__(self, model_fields: list[str], *args, **kwargs):
+    def __init__(self, model_fields: list[str], model_titles: dict[str, str] = {}, *args, **kwargs):
         self.model_fields = model_fields
-        self.model_titles = [field.replace(".", " ").title() for field in model_fields]
+        self.model_titles = [model_titles.get(field, field.replace(".", " ").title()) for field in model_fields]
         super().__init__(*args, **kwargs)
 
     def create_option(
